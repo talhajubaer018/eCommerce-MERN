34dc5daad59300e11b766b8a33e6e8afb63c3eb4
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-09-07 19:18:45 +0600

    adding shopping cart

--- MERN/eCommerce MERN/frontend/src/App.js
+++ MERN/eCommerce MERN/frontend/src/App.js
@@ -1,9 +1,11 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container } from 'react-bootstrap'
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Container>
             <Route path='/' component={HomeScreen} exact />
             <Route path='/product/:id' component={ProductScreen} />
+            <Route path='/cart/:id?' component={CartScreen} />
           </Container>
         </main>
         <Footer/>
--- MERN/eCommerce MERN/frontend/src/screens/ProductScreen.js
+++ MERN/eCommerce MERN/frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Rating from '../components/Rating'
@@ -8,7 +8,9 @@ import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-const ProductScreen = ({match}) => {
+const ProductScreen = ({ history, match }) => {
+  const [quantity, setQuantity] = useState(1)
+
   const dispatch = useDispatch()
 
   const productDetails = useSelector(state => state.productDetails)
@@ -17,6 +19,10 @@ const ProductScreen = ({match}) => {
 useEffect (() => {
   dispatch(listProductDetails(match.params.id))
 }, [dispatch, match])
+
+const addToCartHandler = () => {
+  history.push(`/cart/${match.params.id}?quantity=${quantity}`)
+}
 
  return (
    <>
@@ -33,7 +39,7 @@ useEffect (() => {
             <Rating value={product.rating} text={`${product.numReviews} reviews`} />
           </ListGroup.Item>
           <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-          <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+          <ListGroup.Item>Description: {product.description}</ListGroup.Item>
         </ListGroup>
         </Col>
 
@@ -49,11 +55,29 @@ useEffect (() => {
             <ListGroup.Item>
               <Row>
                 <Col>Status</Col>
-                <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
+                <Col>{product.countInStock }</Col>
               </Row>
             </ListGroup.Item>
+            {product.countInStock > 0 && (
+              <ListGroup.Item>
+                <Row>
+                  <Col>Quantity</Col>
+                  <Col>
+                    <Form.Control className='qty_width' as='select' value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            )}
+
+
             <ListGroup.Item>
-              <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
+              <Button onClick={addToCartHandler} className='btn-block' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
             </ListGroup.Item>
             </ListGroup>
           </Card>
