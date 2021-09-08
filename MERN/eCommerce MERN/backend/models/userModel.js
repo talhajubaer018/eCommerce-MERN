ce21edec4f1f68fabbeec1ba8c1af6c9119beee1
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-09-08 22:58:45 +0600

    Front End User Authentication & Profile

--- MERN/eCommerce MERN/backend/models/userModel.js
+++ MERN/eCommerce MERN/backend/models/userModel.js
@@ -28,7 +28,7 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
-userSchema.pre('save', async (next) => {
+userSchema.pre('save', async function(next) {
 
   if(!this.isModified('password')) {
     next()
