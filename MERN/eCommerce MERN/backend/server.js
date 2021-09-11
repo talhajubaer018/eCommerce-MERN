9adb16aae78697faafb4e1f9310b539a6e1e3ed5
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-09-12 00:58:52 +0600

    Prepare for deployment

--- MERN/eCommerce MERN/backend/server.js
+++ MERN/eCommerce MERN/backend/server.js
@@ -18,10 +18,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Api is running')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -31,7 +27,21 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+
 const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+else {
+  app.get('/', (req, res) => {
+    res.send('Api is running')
+  })
+}
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
